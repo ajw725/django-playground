7dc87a20bdbf562762502c42fa533a6fd252913e
Andrew Weinstein <andrew@altvia.com>
2021-04-04 10:11:02 -0600

    register with decorator

--- users/templatetags/extras.py
+++ users/templatetags/extras.py
@@ -3,11 +3,9 @@ from django import template
 register = template.Library()
 
 
+@register.filter(name='strip_out')
 def strip_out(value, arg):
     """
     This cuts out all values of "arg" from the given string
     """
     return value.replace(arg, '')
-
-
-register.filter('strip_out', strip_out)
